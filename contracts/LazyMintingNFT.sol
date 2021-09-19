c8becc7efc236ecac35cd8a641f70add5d883c98
Daksh Kulshreshtha <akshd@outlook.com>
2021-09-19 14:03:38 +0530

    initial commit

---
+++ contracts/LazyMintingNFT.sol
@@ -0,0 +1,55 @@
+
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.2;
+
+import "@openzeppelin/contracts@4.3.2/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts@4.3.2/token/ERC721/extensions/ERC721Enumerable.sol";
+import "@openzeppelin/contracts@4.3.2/access/Ownable.sol";
+import "@openzeppelin/contracts@4.3.2/utils/Counters.sol";
+
+contract MyNFT is ERC721, ERC721Enumerable, Ownable {
+    using Counters for Counters.Counter;
+
+    Counters.Counter private _tokenIdCounter;
+
+    uint public mintRate =  0.1 ether;
+
+    constructor() ERC721("MyNFT", "DAK") {}
+
+    function _baseURI() internal pure override returns (string memory) {
+        return "https://api.mynft.com/base/";
+    }
+
+    // Anyone can mint tokens if onlyOwner is not there.
+
+    function safeMint(address to) public payable {
+        require(msg.value >= mintRate, "Insufficient funds sent.");
+        _safeMint(to, _tokenIdCounter.current());
+        _tokenIdCounter.increment();
+    }
+
+    // The following functions are overrides required by Solidity.
+
+    function _beforeTokenTransfer(address from, address to, uint256 tokenId)
+        internal
+        override(ERC721, ERC721Enumerable)
+    {
+        super._beforeTokenTransfer(from, to, tokenId);
+    }
+
+    function supportsInterface(bytes4 interfaceId)
+        public
+        view
+        override(ERC721, ERC721Enumerable)
+        returns (bool)
+    {
+        return super.supportsInterface(interfaceId);
+    }
+    
+    // function to be used by owner to withdraw the ether on the contract
+
+    function withdraw() public onlyOwner {
+        require(address(this).balance > 0);
+        payable(owner()).transfer(address(this).balance);
+    }
+}
